198e3dede1ee5edb69609766feb92ae3187b8c4d
NJ-Developer <navsingh191@gmail.com>
2018-08-13 12:44:01 -0400

    unnecessary stuff removed and ex2 updated

--- ud851-Sunshine-student/S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ ud851-Sunshine-student/S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 package com.example.android.sunshine;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -29,69 +28,50 @@ import com.example.android.sunshine.utilities.NetworkUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
-
 public class MainActivity extends AppCompatActivity {
-
     private TextView mWeatherTextView,error;
     private ProgressBar pb;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
-        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
-        pb = (ProgressBar)findViewById(R.id.pb_load);
-        error = (TextView)findViewById(R.id.error);
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
+        pb = findViewById(R.id.pb_load);
+        error =findViewById(R.id.error);
         loadWeatherData();
     }
-    private void loadWeatherData()
-    {
+    private void loadWeatherData(){
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new FetchWeatherTask().execute(location);
     }
-
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>
-    {
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
         @Override
-        protected void onPreExecute()
-        {
-            pb.setVisibility(View.VISIBLE);
+        protected void onPreExecute(){
             super.onPreExecute();
+            pb.setVisibility(View.VISIBLE);
         }
         @Override
         protected String[] doInBackground(String... params) {
-            if (params.length == 0)
-            {
-                return null;
-            }
+            if (params.length == 0)return null;
             String location = params[0];
             URL weatherRequestUrl = NetworkUtils.buildUrl(location);
             try {
-                String jsonWeatherResponse = NetworkUtils
-                        .getResponseFromHttpUrl(weatherRequestUrl);
-
-                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
-                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
-
+                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
                 return simpleJsonWeatherData;
-
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
         }
         @Override
-        protected void onPostExecute(String[] weatherData)
-        {
+        protected void onPostExecute(String[] weatherData){
             pb.setVisibility(View.INVISIBLE);
-            if (weatherData != null)
-            {
-                for (String weatherString : weatherData) {
-                    mWeatherTextView.append((weatherString) + "\n\n\n");
-                }
+            if (weatherData != null){
+                for (String weatherString : weatherData)
+                     mWeatherTextView.append((weatherString) + "\n\n\n");
             }
-            else
-            {
+            else{
                 error.setVisibility(View.VISIBLE);
                 error.setText("Sorry Data not available");
             }
@@ -105,8 +85,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if(id== R.id.action_refresh)
-        {
+        if(id== R.id.action_refresh){
             mWeatherTextView.setText("");
             loadWeatherData();
         }
